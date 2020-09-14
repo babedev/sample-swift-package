ef2bcb41625a9328724cce38a6aa8dcd14ad35bc
Christopher Ng <babedroid@gmail.com>
2020-09-14 16:18:52 +0700

    :fire: Add function

--- Sources/Sample Library/Sample_Library.swift
+++ Sources/Sample Library/Sample_Library.swift
@@ -1,3 +1,11 @@
 struct Sample_Library {
     var text = "Hello, World!"
 }
+
+public class Library {
+    
+    public func yo() -> String {
+        let message = Sample_Library.init()
+        return message.text
+    }
+}
